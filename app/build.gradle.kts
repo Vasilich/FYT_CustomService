d89f058fccd145c519d1c3dd2f96fd9827f203c0
Vasilich <igor.abakumov@gmail.com>
2026-03-28 21:28:44 +0100

    Reduce APK size by removing unused Material dependency and switching to AppCompat theme

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,7 +50,6 @@ android {
 dependencies {
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.work:work-runtime-ktx:2.9.1")
 }
